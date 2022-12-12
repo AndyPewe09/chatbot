494148a8579044abd19683e98de58e2f8edd5a9d
AndyPew <christianandyp09@gmail.com>
2022-12-12 18:16:23 +0700

    chatbot

---
+++ chatbot/constants.py
@@ -0,0 +1,16 @@
+FIRST_QUESTIONS = {
+    "en": "Hi, how are you?",
+    "de": "Hallo, wie geht es dir?",
+    "pt-br": "Oi como você está?",
+    "he": "היי מה קורה?",
+    "id": "Hallo, bagaimana kabarmu?"
+}
+TERMINATES = {
+    "en": "quit",
+    "de": "ende",
+    "pt-br": "sair",
+    "he": "זהו",
+    "id": "keluar"
+}
+
+LANGUAGE_SUPPORT = ["en", "de", "pt-br", "he","id"]
